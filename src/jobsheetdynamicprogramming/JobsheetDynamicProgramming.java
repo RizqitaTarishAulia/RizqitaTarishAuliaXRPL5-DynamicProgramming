167cc937a2f598ac292b18348788017e517fe2c2
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-09-12 13:04:44 +0700

    Tambah Hitung Fibonacci

--- src/jobsheetdynamicprogramming/JobsheetDynamicProgramming.java
+++ src/jobsheetdynamicprogramming/JobsheetDynamicProgramming.java
@@ -5,6 +5,7 @@
  */
 package jobsheetdynamicprogramming;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -27,6 +28,7 @@ public class JobsheetDynamicProgramming {
             String identitas = "Rizqita Tarish Aulia / XRPL5 / 37";
             tampilJudul(identitas);
             int n = tampilInput();
+            BigInteger hasil = fibo(n);
         }
         private static int tampilInput(){
             Scanner scanner = new Scanner(System.in);
@@ -35,6 +37,18 @@ public class JobsheetDynamicProgramming {
             int n = scanner.nextInt();
             
             return n;
+        }
+        private static BigInteger fibo(int n){
+            
+            BigInteger[] hasil = new BigInteger[n];
+            
+            hasil[0] = BigInteger.ONE;
+            hasil[1] = BigInteger.ONE;
+            
+            for(int i = 2; i < n; i++){
+                hasil[i] = hasil[i-1].add(hasil[1-2]);
+            }
+            return hasil[n-1];
         }
     }
     
