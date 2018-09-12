dde70e7796e0997eb4750ec04a882739c13cc167
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-09-12 13:16:49 +0700

    Tambah Tampil Hasil

--- src/jobsheetdynamicprogramming/JobsheetDynamicProgramming.java
+++ src/jobsheetdynamicprogramming/JobsheetDynamicProgramming.java
@@ -29,6 +29,7 @@ public class JobsheetDynamicProgramming {
             tampilJudul(identitas);
             int n = tampilInput();
             BigInteger hasil = fibo(n);
+            tampilHasil(n, hasil);
         }
         private static int tampilInput(){
             Scanner scanner = new Scanner(System.in);
@@ -45,10 +46,13 @@ public class JobsheetDynamicProgramming {
             hasil[0] = BigInteger.ONE;
             hasil[1] = BigInteger.ONE;
             
-            for(int i = 2; i < n; i++){
-                hasil[i] = hasil[i-1].add(hasil[1-2]);
+            for(int i =2; i<n; i++){
+                hasil[i] = hasil[i-1].add(hasil[i-2]);
             }
             return hasil[n-1];
+        }
+        private static void tampilHasil(int n, BigInteger hasil){
+            System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
         }
     }
     
