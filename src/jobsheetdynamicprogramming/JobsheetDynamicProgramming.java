de6eed4020aa30cb662c7e558068d39df9b215ee
Tarishaulia <Tarishaulia@DESKTOP-88IEU4V>
2018-09-12 12:53:05 +0700

    Initial Commit

---
+++ src/jobsheetdynamicprogramming/JobsheetDynamicProgramming.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheetdynamicprogramming;
+
+/**
+ *
+ * @author Tarishaulia
+ */
+public class JobsheetDynamicProgramming {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static void tampilJudul(String identitas){
+        // TODO code application logic here
+        System.out.println("Identitas : "+identitas);
+        
+        System.out.println("\nHiutng Fibonacci");
+        System.out.println("1,1,2,3,5,8,13,21,... dst.\n");
+    }
+        public static void main(String[] args){
+            String identitas = "Rizqita Tarish Aulia / XRPL5 / 37";
+            tampilJudul(identitas);
+        }
+    }
+    
+
